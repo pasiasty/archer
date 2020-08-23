f5bcba4bec67de43722c63a15f1893dcb68a6f56
Mariusz Pasek <mariusz.pasek@aeolusbot.com>
2020-08-23 21:14:20 +0200

    excalibur minimum app

--- assets/js/application.js
+++ assets/js/application.js
@@ -2,6 +2,29 @@ require("expose-loader?$!expose-loader?jQuery!jquery");
 require("bootstrap/dist/js/bootstrap.bundle.js");
 require("@fortawesome/fontawesome-free/js/all.js");
 
-$(() => {
+ex = require("excalibur/dist/excalibur.min.js");
 
-});
+// Create an instance of the engine.
+const game = new ex.Engine({
+    width: 800,
+    height: 600,
+  })
+
+// Create an actor with x position of 150px,
+// y position of 40px from the bottom of the screen,
+// width of 200px, height and a height of 20px
+const paddle = new ex.Actor(150, game.drawHeight - 40, 200, 20)
+
+// Let's give it some color with one of the predefined
+// color constants
+paddle.color = ex.Color.Chartreuse
+
+// Make sure the paddle can partipate in collisions, by default excalibur actors do not collide
+paddle.collisionType = ex.CollisionType.Fixed
+
+// `game.add` is the same as calling
+// `game.currentScene.add`
+game.add(paddle)
+
+// Start the engine to begin the game.
+game.start()
--- go.mod
+++ go.mod
@@ -1,3 +1,15 @@
 module github.com/pasiasty/archer2
 
 go 1.13
+
+require (
+	github.com/gobuffalo/buffalo v0.16.14
+	github.com/gobuffalo/envy v1.9.0
+	github.com/gobuffalo/mw-csrf v1.0.0
+	github.com/gobuffalo/mw-forcessl v0.0.0-20200131175327-94b2bd771862
+	github.com/gobuffalo/mw-i18n v1.1.0
+	github.com/gobuffalo/mw-paramlogger v1.0.0
+	github.com/gobuffalo/packr/v2 v2.8.0
+	github.com/gobuffalo/suite v2.8.2+incompatible
+	github.com/unrolled/secure v1.0.8
+)
