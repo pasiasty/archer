548642521a2d3c1a8d4911cba9361d62927723d7
Mariusz Pasek <pasiasty@gmail.com>
2020-09-05 00:10:01 +0200

    added particle emitter for arrow

--- assets/src/arrow.ts
+++ assets/src/arrow.ts
@@ -11,6 +11,7 @@ export class Arrow extends ex.Actor {
     callback: (game: ex.Engine, collidedWith: string) => void
     accDelta: number
     keepFlying: boolean
+    emitter: ex.ParticleEmitter
 
     constructor(game: ex.Engine, callback: (game: ex.Engine, collidedWith: string) => void, trajectory: Trajectory, color: ex.Color) {
         super()
@@ -29,13 +30,37 @@ export class Arrow extends ex.Actor {
         this.game = game
         this.callback = callback
 
+        this.emitter = new ex.ParticleEmitter();
+        this.emitter.emitterType = ex.EmitterType.Rectangle;
+        this.emitter.radius = 5;
+        this.emitter.minVel = 100;
+        this.emitter.maxVel = 200;
+        this.emitter.minAngle = 3.2;
+        this.emitter.maxAngle = 3.6;
+        this.emitter.isEmitting = true;
+        this.emitter.emitRate = 50;
+        this.emitter.opacity = 0.5;
+        this.emitter.fadeFlag = true;
+        this.emitter.particleLife = 400;
+        this.emitter.maxSize = 10;
+        this.emitter.minSize = 1;
+        this.emitter.startSize = 0;
+        this.emitter.endSize = 0;
+        this.emitter.acceleration = new ex.Vector(800, 0);
+        this.emitter.beginColor = ex.Color.Orange;
+        this.emitter.endColor = ex.Color.Yellow;
+
         this.game.add(this)
+        this.game.add(this.emitter)
+        this.setZIndex(30)
+        this.emitter.setZIndex(10)
     }
 
     public onPostUpdate(engine: ex.Engine, delta: number) {
         if (!this.keepFlying)
             return
         if (this.sampleIdx == this.trajectory.ArrowStates.length) {
+            this.emitter.kill()
             this.keepFlying = false
             if (this.trajectory.CollidedWith != "planet") {
                 this.game.remove(this)
@@ -52,6 +77,10 @@ export class Arrow extends ex.Actor {
             var arrowState = this.trajectory.ArrowStates[this.sampleIdx]
             this.pos = new ex.Vector(arrowState.Position.X, arrowState.Position.Y)
             this.rotation = arrowState.Orientation
+
+            this.emitter.pos = this.pos.sub(new ex.Vector(Math.cos(this.rotation) * 55, Math.sin(this.rotation) * 55))
+            this.emitter.rotation = this.rotation
+
             this.sampleIdx++
         }
     }
